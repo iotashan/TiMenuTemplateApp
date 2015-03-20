ccd2d88296123fe8d9c9f93b34e87d046e66de13
Shannon Hicks <shan@iotashan.com>
2015-03-20 06:24:49 -0500

    Duh, tishadow doesn't need a commonjs test

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -1,27 +1,21 @@
-module.exports = function() {
+describe('index.js', function() {
+	var Alloy = require("alloy");
+	var $;
 
-    describe('index.js', function() {
-		var Alloy = require("alloy");
-		var $;
-	
-	
-		beforeEach(function() {
-			$ = Alloy.createController("index");
-		});
-	
-		it("should create a global nav controller", function() {
-			Alloy.Globals.should.have.property('navController');
-		});
-	
-		it("should create the menu UI", function() {
-			Alloy.Globals.should.have.property('menu');
-		});
-	
-		it("should create the sliding drawer", function() {
-			Alloy.Globals.should.have.property('drawer');
-		});
-    });
 
-    // run the tests
-    mocha.run();
-};
+	beforeEach(function() {
+		$ = Alloy.createController("index");
+	});
+
+	it("should create a global nav controller", function() {
+		Alloy.Globals.should.have.property('navController');
+	});
+
+	it("should create the menu UI", function() {
+		Alloy.Globals.should.have.property('menu');
+	});
+
+	it("should create the sliding drawer", function() {
+		Alloy.Globals.should.have.property('drawer');
+	});
+});
